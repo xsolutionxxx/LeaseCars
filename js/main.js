3f05a63284cbc8b00e8af610aab322c12bad1b40
Nazar Talaievych <losiara345@gmail.com>
2024-06-04 23:57:27 +0300

    needs updating

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const tabItem = document.querySelectorAll ('.tabs__btn-item'),
+	  tabConten = document.querySelectorAll ('.tabs__content-item');
+
+tabItem.forEach(function(element) {
+	element.addEventListener('click', open);
+})
+
+function open(evt) {
+	const tabTarget = evt.currentTarget;
+	const button = tabTarget.dataset.button;
+
+	tabItem.forEach(function(item) {
+		item.classList.remove('tabs__btn-item--active');
+	});
+
+	tabTarget.classList.add('tabs__btn-item--active');
+
+	tabConten.forEach(function(item) {
+		item.classList.remove('tabs__content-item--active');
+	});
+
+	document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
+}
+
+
+const swiper = new Swiper(".swiper", {
+	effect: "fade",
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false,
+	},
+	pagination: {
+		el: ".swiper-pagination"
+	}
+});
